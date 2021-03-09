8fa96a92759f3bc09ff6f4b4c266f5dc196ef52a
chrystalquek <chrystalquek@yahoo.com.sg>
2021-03-09 17:58:18 +0800

    Initial Commit

---
+++ ChopChop/ChopChopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChopChopApp.swift
+//  ChopChop
+//
+//  Created by Chrystal on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ChopChopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
