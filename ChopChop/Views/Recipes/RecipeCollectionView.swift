ab747e0c6e92ce55e65ce1c3a7e0dd6ab4d0840b
Seow Alex <seowalex@hotmail.com>
2021-03-20 17:56:29 +0800

    Update RecipeCollectionView

--- ChopChop/Views/Recipes/RecipeCollectionView.swift
+++ ChopChop/Views/Recipes/RecipeCollectionView.swift
@@ -7,34 +7,53 @@ struct RecipeCollectionView: View {
     var body: some View {
         VStack {
             SearchBar(text: $viewModel.query, placeholder: "Search recipes...")
-            IngredientFilter()
-                .padding([.trailing, .leading])
+//            IngredientFilter()
+//                .padding([.trailing, .leading])
             List(viewModel.recipes) { recipe in
-                NavigationLink(
-                    destination: Text(recipe.name)
-                ) {
-                    HStack(alignment: .top) {
-                        Image("recipe")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 80, height: 80)
-                            .cornerRadius(10)
-                            .clipped()
-                        VStack(alignment: .leading) {
-                            Text(recipe.name)
-                            Text("Absolutely delish")
-                                .font(.caption)
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    .padding([.top, .bottom], 8)
-                }
+                RecipeRow(recipe: recipe)
             }
         }
         .navigationTitle(Text(viewModel.title))
         .onDisappear {
             viewModel.query = ""
             viewModel.selectedIngredients.removeAll()
+        }
+    }
+
+    func RecipeRow(recipe: RecipeInfo) -> some View {
+        NavigationLink(
+            destination: Text(recipe.name)
+        ) {
+            HStack(alignment: .top) {
+                Image("recipe")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 80, height: 80)
+                    .cornerRadius(10)
+                    .clipped()
+                VStack(alignment: .leading) {
+                    let servingNumber = Int.random(in: 1...4)
+
+                    Text(recipe.name)
+                    VStack(alignment: .leading) {
+                        Text("Serves \(servingNumber)-\(servingNumber + Int.random(in: 1...2)) people")
+                        HStack(spacing: 0) {
+                            let rating = Int.random(in: 2...6)
+
+                            ForEach(1..<rating) { _ in
+                                Image(systemName: "star.fill")
+                            }
+
+                            ForEach(rating..<6) { _ in
+                                Image(systemName: "star")
+                            }
+                        }
+                    }
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                }
+            }
+            .padding([.top, .bottom], 6)
         }
     }
 
@@ -47,7 +66,7 @@ struct RecipeCollectionView: View {
                 if viewModel.selectedIngredients.isEmpty {
                     Text("Filter by ingredients...")
                 } else {
-                    Text(viewModel.selectedIngredients.joined(separator: ", "))
+                    Text(Array(viewModel.selectedIngredients).sorted().joined(separator: ", "))
                 }
             }
             .popover(isPresented: $showingPopover) {
