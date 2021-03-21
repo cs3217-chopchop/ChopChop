cb0b3105a222a20c8d31895974aac68f08f107ea
Siau Chiak <gsc928@gmail.com>
2021-03-21 14:56:15 +0800

    Add VolumeUnit tests

--- ChopChop/Models/Ingredients/Date+Extensions.swift
+++ ChopChop/Models/Ingredients/Date+Extensions.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension Date {
     static let today: Date = Calendar.current.startOfDay(for: Date())
-    
+
     var startOfDay: Date {
         Calendar.current.startOfDay(for: self)
     }
