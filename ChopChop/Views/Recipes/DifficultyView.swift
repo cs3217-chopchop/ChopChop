c4be304af1f07c428d2c264f63f9d95658e6495d
Seow Alex <seowalex@hotmail.com>
2021-03-25 19:19:24 +0800

    Fix difficulty not updating

--- ChopChop/Views/Recipes/DifficultyView.swift
+++ ChopChop/Views/Recipes/DifficultyView.swift
@@ -6,11 +6,11 @@ struct DifficultyView: View {
     var body: some View {
 
         if let difficulty = difficulty {
-            ForEach(0..<difficulty.rawValue) { _ in
+            ForEach(0..<difficulty.rawValue, id: \.self) { _ in
                 Image(systemName: "star.fill")
             }
 
-            ForEach(difficulty.rawValue..<5) { _ in
+            ForEach(difficulty.rawValue..<5, id: \.self) { _ in
                 Image(systemName: "star")
             }
         } else {
