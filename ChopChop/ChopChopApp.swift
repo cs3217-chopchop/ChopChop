916dfce19bb49a22cfaab7ec2e28c0f22d4049d9
shadowezz <wenjie.cao@hotmail.com>
2021-03-27 10:20:02 +0800

    Integrate firebase sdk.

--- ChopChop/ChopChopApp.swift
+++ ChopChop/ChopChopApp.swift
@@ -1,9 +1,10 @@
 import SwiftUI
+import Firebase
 
 @main
 struct ChopChopApp: App {
     @StateObject var settings = UserSettings()
-
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     var body: some Scene {
         WindowGroup {
             NavigationView {
@@ -12,4 +13,13 @@ struct ChopChopApp: App {
             .environmentObject(settings)
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
