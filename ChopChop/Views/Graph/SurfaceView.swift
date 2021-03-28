d49b897e7adb5bf03b5a8fdccb2b6728645c9295
Seow Alex <seowalex@hotmail.com>
2021-03-28 14:19:13 +0800

    Add edge adding

--- ChopChop/Views/Graph/SurfaceView.swift
+++ ChopChop/Views/Graph/SurfaceView.swift
@@ -15,73 +15,125 @@ struct SurfaceView: View {
 
     @State var dragOffset2: CGSize = .zero
     @State var selectedNode: Node?
+    @GestureState var dragInfo: DragInfo?
+
+    struct DragInfo {
+        var from: CGPoint
+        var to: CGPoint
+    }
 
     var offset: CGVector {
         CGVector(dx: portalPosition.x + dragOffset.width, dy: portalPosition.y + dragOffset.height)
     }
 
     var body: some View {
-        ZStack {
-            ForEach(viewModel.graph.edgeList(), id: \.description) { edge in
-                Line(from: viewModel.graph.vertexAtIndex(edge.u).position + offset
-                        + (selectedNode == viewModel.graph.vertexAtIndex(edge.u)
-                            ? CGVector(dx: dragOffset2.width, dy: dragOffset2.height) : .zero),
-                     to: viewModel.graph.vertexAtIndex(edge.v).position + offset
-                        + (selectedNode == viewModel.graph.vertexAtIndex(edge.v)
-                            ? CGVector(dx: dragOffset2.width, dy: dragOffset2.height) : .zero))
-                    .stroke(Color.blue, lineWidth: 3)
-            }
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(viewModel.graph.edgeList(), id: \.description) { edge in
+                    Line(from: viewModel.graph.vertexAtIndex(edge.u).position + offset
+                            + (selectedNode == viewModel.graph.vertexAtIndex(edge.u)
+                                ? CGVector(dx: dragOffset2.width, dy: dragOffset2.height) : .zero),
+                         to: viewModel.graph.vertexAtIndex(edge.v).position + offset
+                            + (selectedNode == viewModel.graph.vertexAtIndex(edge.v)
+                                ? CGVector(dx: dragOffset2.width, dy: dragOffset2.height) : .zero))
+                        .stroke(Color.blue, lineWidth: 3)
+                }
 
-            if let nodes = viewModel.graph.topologicalSort() {
-                ForEach(nodes) { node in
-                    NodeView(selection: selection, node: node, index: 1)
-                        .position(node.position + offset
-                                    + (selectedNode == node
-                                        ? CGVector(dx: dragOffset2.width, dy: dragOffset2.height)
-                                        : .zero))
-                        .onTapGesture {
-                            selection.toggleNode(node)
-                        }
-                        .gesture(
-                            DragGesture()
-                                .onChanged { value in
-                                    dragOffset2 = value.translation
-                                    selectedNode = node
-                                }
-                                .onEnded { value in
-                                    dragOffset2 = .zero
-                                    selectedNode = nil
-                                    node.position += CGVector(dx: value.translation.width,
-                                                              dy: value.translation.height)
-                                }
-                        )
+                if let info = dragInfo {
+                    Line(from: info.from, to: info.to)
+                        .stroke(Color.red, lineWidth: 3)
+                }
+
+                if let nodes = viewModel.graph.topologicalSort() {
+                    ForEach(nodes) { node in
+                        NodeView(selection: selection, node: node, index: 1)
+                            .position(node.position + offset
+                                        + (selectedNode == node
+                                            ? CGVector(dx: dragOffset2.width, dy: dragOffset2.height)
+                                            : .zero))
+                            .onTapGesture {
+                                selection.toggleNode(node)
+                            }
+                            .gesture(
+                                LongPressGesture()
+                                    .onEnded { _ in
+                                        print("move!")
+                                    }
+                                    .sequenced(before:
+                                        DragGesture()
+                                                .updating($dragInfo) { value, state, _ in
+                                                    state = DragInfo(from: node.position + CGVector(dx: portalPosition.x,
+                                                                                                    dy: portalPosition.y),
+                                                                     to: value.location)
+                                                }
+                                                .onEnded { value in
+                                                    print(value.location)
+                                                    if let targetNode = hitTest(point: value.location, parent: geometry.size) {
+                                                        viewModel.graph.addEdge(from: node, to: targetNode, directed: true)
+                                                    }
+                                                }
+                                    )
+                                    .exclusively(before:
+                                        DragGesture()
+                                            .onChanged { value in
+                                                dragOffset2 = value.translation
+                                                selectedNode = node
+                                            }
+                                            .onEnded { value in
+                                                dragOffset2 = .zero
+                                                selectedNode = nil
+                                                node.position += CGVector(dx: value.translation.width,
+                                                                          dy: value.translation.height)
+                                            }
+                                )
+                            )
+                    }
                 }
             }
-        }
-        .contentShape(Rectangle())
-        .background(Color.orange)
-        .onTapGesture {
-            selection.unselectAllNodes()
-        }
-        .gesture(
-            LongPressGesture().sequenced(before: DragGesture(minimumDistance: 0).onEnded { value in
-                viewModel.graph.addVertex(Node(position: value.location - CGVector(dx: portalPosition.x,
-                                                                                   dy: portalPosition.y),
-                                               text: "Lorem ipsum"))
-                selection.objectWillChange.send()
-            }).exclusively(before:
-                DragGesture()
-                    .onChanged { value in
-                        dragOffset = value.translation
-                    }
-                    .onEnded { value in
-                        dragOffset = .zero
+            .contentShape(Rectangle())
+            .background(Color.orange)
+            .onTapGesture {
+                selection.unselectAllNodes()
+            }
+            .gesture(
+                LongPressGesture().sequenced(before: DragGesture(minimumDistance: 0).onEnded { value in
+                    viewModel.graph.addVertex(Node(position: value.location - CGVector(dx: portalPosition.x,
+                                                                                       dy: portalPosition.y),
+                                                   text: "Lorem ipsum"))
+                    selection.objectWillChange.send()
+                }).exclusively(before:
+                    DragGesture()
+                        .onChanged { value in
+                            dragOffset = value.translation
+                        }
+                        .onEnded { value in
+                            dragOffset = .zero
 
-                        portalPosition = CGPoint(x: portalPosition.x + value.translation.width,
-                                                 y: portalPosition.y + value.translation.height)
-                    }
+                            portalPosition = CGPoint(x: portalPosition.x + value.translation.width,
+                                                     y: portalPosition.y + value.translation.height)
+                        }
+                )
             )
-        )
+        }
+    }
+}
+
+extension SurfaceView {
+    func hitTest(point: CGPoint, parent: CGSize) -> Node? {
+        for node in viewModel.graph.vertices {
+            let endPoint = CGPoint(x: node.position.x + portalPosition.x - 60,
+                                   y: node.position.y + portalPosition.y - 40)
+            print("endPoint: \(endPoint)")
+            let rect = CGRect(origin: endPoint,
+                              size: CGSize(width: 120, height: 80))
+
+            if rect.contains(point) {
+                print(node.text)
+                return node
+            }
+        }
+
+        return nil
     }
 }
 
