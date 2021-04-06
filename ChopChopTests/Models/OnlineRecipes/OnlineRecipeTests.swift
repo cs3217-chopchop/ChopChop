87b36294ad3a7515ef9de7d291ecf2bb2aff558e
chrystalquek <chrystalquek@yahoo.com.sg>
2021-04-06 12:10:28 +0800

    Merge OnlineRecipeTests

--- ChopChopTests/Models/OnlineRecipes/OnlineRecipeTests.swift
+++ ChopChopTests/Models/OnlineRecipes/OnlineRecipeTests.swift
@@ -1,16 +1,32 @@
-//
-//  OnlineRecipeTest.swift
-//  ChopChopTests
-//
-//  Created by Cao Wenjie on 6/4/21.
-//
+// swiftlint:disable line_length
 
 import XCTest
 @testable import ChopChop
 
-class OnlineRecipeTest: XCTestCase {
+class OnlineRecipeTests: XCTestCase {
 
-    func testDefaultInitializer_allFieldsFilled_success() throws {
+    func testConstruct() throws {
+        let onlineRecipe = try OnlineRecipe(id: "1", userId: "1", name: "Pancakes", servings: 2, difficulty: Difficulty.easy, cuisine: nil, steps: [], stepGraph: RecipeStepGraph(), ingredients: [], ratings: [], created: Date())
+
+        XCTAssertEqual(onlineRecipe.id, "1")
+        XCTAssertEqual(onlineRecipe.userId, "1")
+        XCTAssertEqual(onlineRecipe.name, "Pancakes")
+        XCTAssertEqual(onlineRecipe.servings, 2)
+        XCTAssertEqual(onlineRecipe.difficulty, Difficulty.easy)
+        XCTAssertNil(onlineRecipe.cuisine)
+        XCTAssertTrue(onlineRecipe.steps.isEmpty)
+        XCTAssertEqual(onlineRecipe.stepGraph, RecipeStepGraph())
+        XCTAssertTrue(onlineRecipe.ingredients.isEmpty)
+        XCTAssertTrue(onlineRecipe.ratings.isEmpty)
+    }
+
+    func testConstruct_fail() throws {
+        XCTAssertThrowsError(try OnlineRecipe(id: "1", userId: "1", name: "        ", servings: 2, difficulty: nil, cuisine: nil, steps: [], stepGraph: RecipeStepGraph(), ingredients: [], ratings: [], created: Date()))
+
+        XCTAssertThrowsError(try OnlineRecipe(id: "1", userId: "1", name: "Pancakes", servings: 0, difficulty: nil, cuisine: nil, steps: [], stepGraph: RecipeStepGraph(), ingredients: [], ratings: [], created: Date()))
+    }
+
+    func testConstruct_allFieldsFilled_success() throws {
         let steps = ["First step", "Second step", "Third Step"]
         let stepNodes = try steps.map({ RecipeStepNode(try RecipeStep(content: $0)) })
         let stepEdges = [
@@ -54,7 +70,7 @@ class OnlineRecipeTest: XCTestCase {
         XCTAssertEqual(onlineRecipe.created, currentDate)
     }
 
-    func testDefaultInitializer_optionalFieldsNotFilled_success() throws {
+    func testConstruct_optionalFieldsNotFilled_success() throws {
         let steps = ["First step", "Second step", "Third Step"]
         let stepNodes = try steps.map({ RecipeStepNode(try RecipeStep(content: $0)) })
         let stepEdges = [
@@ -98,8 +114,7 @@ class OnlineRecipeTest: XCTestCase {
         XCTAssertEqual(onlineRecipe.created, currentDate)
     }
 
-    func testDefaultInitializer_emptyName_fail() throws {
-
+    func testConstruct_emptyName_fail() throws {
         let steps = ["First step", "Second step", "Third Step"]
         let stepNodes = try steps.map({ RecipeStepNode(try RecipeStep(content: $0)) })
         let stepEdges = [
@@ -132,7 +147,7 @@ class OnlineRecipeTest: XCTestCase {
         ))
     }
 
-    func testDefaultInitializer_servingBelowZero_fail() throws {
+    func testConstruct_servingBelowZero_fail() throws {
         let steps = ["First step", "Second step", "Third Step"]
         let stepNodes = try steps.map({ RecipeStepNode(try RecipeStep(content: $0)) })
         let stepEdges = [
@@ -221,4 +236,19 @@ class OnlineRecipeTest: XCTestCase {
         XCTAssertEqual(onlineRecipe.created, currentDate)
 
     }
+
+    func testConvenienceInitializer() throws {
+        let onlineRecipe = try OnlineRecipe(from: OnlineRecipeRecord(id: "1", name: "Pancakes", creator: "1", servings: 2, ingredients: [OnlineIngredientRecord(name: "Butter", quantity: .count(2))], steps: ["Cook the pancakes", "Make the pancakes"], stepEdges: [OnlineStepEdgeRecord(sourceStep: "Cook the pancakes", destinationStep: "Make the pancakes")], created: Date()))
+
+        XCTAssertEqual(onlineRecipe.name, "Pancakes")
+        XCTAssertEqual(onlineRecipe.servings, 2)
+        XCTAssertEqual(onlineRecipe.ingredients, [try RecipeIngredient(name: "Butter", quantity: Quantity(.count, value: 2))])
+        XCTAssertEqual(onlineRecipe.steps, ["Cook the pancakes", "Make the pancakes"])
+
+        let node1 = RecipeStepNode(try RecipeStep(content: "Cook the pancakes"))
+        let node2 = RecipeStepNode(try RecipeStep(content: "Make the pancakes"))
+        let edges = [Edge(source: node1, destination: node2)].compactMap { $0 }
+        XCTAssertEqual(onlineRecipe.stepGraph, try RecipeStepGraph(nodes: [node1, node2], edges: edges))
+    }
+
 }
