149f6a834757483fec75624446538be1a27047af
chrystalquek <chrystalquek@yahoo.com.sg>
2021-04-09 21:04:11 +0800

    Create OnlineRecipeInfoRecord

---
+++ ChopChop/Storage/Models/OnlineRecipes/OnlineRecipeInfoRecord.swift
@@ -0,0 +1,16 @@
+import Foundation
+import FirebaseFirestoreSwift
+
+struct OnlineRecipeInfoRecord {
+    @DocumentID var id: String? // same as OnlineRecipe id
+    var creator: String // creatorId
+    @ServerTimestamp var createdAt: Date?
+    @ServerTimestamp var updatedAt: Date?
+}
+
+extension OnlineRecipeInfoRecord: Equatable {
+}
+
+extension OnlineRecipeInfoRecord: Codable {
+}
+
