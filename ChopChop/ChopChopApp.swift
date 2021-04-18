53406b75184aacb4b57d85c3eae6ecbaefa15867
Seow Alex <seowalex@hotmail.com>
2021-04-18 18:33:13 +0800

    Improve welcome screen

--- ChopChop/ChopChopApp.swift
+++ ChopChop/ChopChopApp.swift
@@ -22,10 +22,10 @@ struct ChopChopApp: App {
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+            FirebaseApp.configure()
+            return true
+    }
 }
