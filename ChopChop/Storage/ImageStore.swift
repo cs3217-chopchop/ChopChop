353484f0390fab1b1228f95c624acd39ad2195e9
Siau Chiak <gsc928@gmail.com>
2021-04-19 23:15:42 +0800

    Add documentation to image store

--- ChopChop/Storage/ImageStore.swift
+++ ChopChop/Storage/ImageStore.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 /**
+ A class that manages the persistence of images into local storage.
  Reference: https://gist.github.com/TheCodedSelf/7ff3a4fb64f8f6131925fa3e6e21efbe
  */
 struct ImageStore {
     static let fileManager = FileManager.default
 
+    /**
+     Deletes the image with the given name in the folder with the given folder name from local storage.
+     If such an image or folder does not exist, do nothing.
+     */
     static func delete(imageNamed name: String, inFolderNamed folderName: String = "") {
         guard let imagePath = ImageStore.getFilePath(for: name, folderName: folderName) else {
             return
@@ -14,12 +19,19 @@ struct ImageStore {
         try? ImageStore.fileManager.removeItem(at: imagePath)
     }
 
+    /**
+     Deletes all images with names in the given array of names from the folder with the given folder name.
+     */
     static func delete(imagesNamed names: [String], inFolderNamed folderName: String = "") {
         for name in names {
             delete(imageNamed: name, inFolderNamed: folderName)
         }
     }
 
+    /**
+     Deletes all images in the folder with the given folder name.
+     If such a folder does not exist, do nothing.
+     */
     static func deleteAll(inFolderNamed folderName: String = "") {
         guard let folderPath = try? ImageStore.fileManager.url(for: .documentDirectory,
                                                                in: .userDomainMask,
@@ -40,6 +52,10 @@ struct ImageStore {
         }
     }
 
+    /**
+     Returns the image with the given name from the folder with the given folder name,
+     or `nil` if such an image does not exist.
+     */
     static func fetch(imageNamed name: String, inFolderNamed folderName: String = "") -> UIImage? {
         guard let imagePath = ImageStore.getFilePath(for: name, folderName: folderName) else {
             return nil
@@ -49,6 +65,13 @@ struct ImageStore {
         return image
     }
 
+    /**
+     Saves the given image with the given name into the folder with the given folder name.
+
+     - Throws:
+        - `ImageStoreError.imageCreationFailure` if the given image could not be converted into PNG data.
+        - `ImageStoreError.pathCreationFailure` if the path could not be created with the given image name and folder name.
+     */
     static func save(image: UIImage, name: String, inFolderNamed folderName: String = "") throws {
         guard let imageData = image.png() else {
             throw ImageStoreError.imageCreationFailure
@@ -65,6 +88,7 @@ struct ImageStore {
         for imageName: String,
         folderName: String = "",
         fileExtension: String = "png") -> URL? {
+
         guard !imageName.isEmpty else {
             return nil
         }
@@ -95,13 +119,16 @@ enum ImageStoreError: Error {
     case pathCreationFailure
 }
 
-// MARK: - PNG data with correct orientation
+// MARK: - Orientate Image
 extension UIImage {
+    /**
+     Converts the given image into PNG data with the correct orientation..
+     */
     func png(isOpaque: Bool = true) -> Data? {
         flattened(isOpaque: isOpaque).pngData()
     }
 
-    func flattened(isOpaque: Bool = true) -> UIImage {
+    private func flattened(isOpaque: Bool = true) -> UIImage {
         if imageOrientation == .up {
             return self
         }
